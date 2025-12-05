7f18caff4f2af37556b17f395e6ec9d57dfc799b
nainJaramillo <aratenorio6@gmail.com>
2025-12-05 13:22:09 +0100

    .

--- src/main/java/com/PooIntro/MainTareaSimple.java
+++ src/main/java/com/PooIntro/MainTareaSimple.java
@@ -2,26 +2,20 @@ package com.PooIntro;
 
 public class MainTareaSimple {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TareaSimple objeto1 = new TareaSimple();
-		objeto1.setTitulo = "Nueva Tarea";
-		System.out.println(objeto1.getTitulo);
-		
-		TareaSimple objeto2 = new TareaSimple("Otra tarea", false);
-		System.out.println(objeto2.titulo);
-	
-		TareaSimple objeto3 = new TareaSimple("demo",true);
-		objeto3.MostrarTarea();
-		
-	 
-		
-		
-		
-		
-		
-		
-		
-	}
+
+public static void main(String[] args) {
+        TareaSimple objeto1 = new TareaSimple();
+        objeto1.setTitulo("Nueva Tarea");
+        System.out.println(objeto1.getTitulo());
+
+        TareaSimple objeto2 = new TareaSimple("Otra tarea", false);
+        System.out.println(objeto2.getTitulo());
+
+        TareaSimple objeto3 = new TareaSimple("Tercera tarea", true);
+        objeto3.mostrarTarea();
+        
+        
+    }
+
 
 }
